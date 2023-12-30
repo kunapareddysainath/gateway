d212b4fd0793d58e69ee86d39c87c7248b801f07
sainath2311 <145591166+kunapareddysainath@users.noreply.github.com>
2023-12-30 23:05:37 +0530

    first commit

---
+++ gateway/src/test/java/in/workbyte/wbimsgateway/WbImsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package in.workbyte.wbimsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbImsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
